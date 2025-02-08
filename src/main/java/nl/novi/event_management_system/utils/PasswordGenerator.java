5c6c7ee9d8e7517faf0db6770982adecacdaf9ab
Tarek <tarek_tarho@hotmail.com>
2025-02-08 09:43:58 -0500

    Adding DcoString

--- src/main/java/nl/novi/event_management_system/utils/PasswordGenerator.java
+++ src/main/java/nl/novi/event_management_system/utils/PasswordGenerator.java
@@ -4,6 +4,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordGenerator {
 
+
+    /**
+     * Generates an encrypted password using BCrypt hashing algorithm.
+     *
+     * @param password the plain text password to be encrypted
+     * @return the encrypted password
+     */
     public static String generateEncryptedPassword(String password) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
         return encoder.encode(password);
