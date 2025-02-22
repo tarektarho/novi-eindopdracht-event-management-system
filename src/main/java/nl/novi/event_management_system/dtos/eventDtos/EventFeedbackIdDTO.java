2c9fa8368dbad2c60ec6bc27fa80b830e6d255eb
Tarek <tarek_tarho@hotmail.com>
2025-02-22 22:45:43 +0100

    fix: add Lombok annotations for constructors in DTOs

--- src/main/java/nl/novi/event_management_system/dtos/eventDtos/EventFeedbackIdDTO.java
+++ src/main/java/nl/novi/event_management_system/dtos/eventDtos/EventFeedbackIdDTO.java
@@ -1,6 +1,7 @@
 package nl.novi.event_management_system.dtos.eventDtos;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.util.UUID;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class EventFeedbackIdDTO {
     @NotNull(message = "feedback ID is mandatory")
     private UUID feedbackId;
